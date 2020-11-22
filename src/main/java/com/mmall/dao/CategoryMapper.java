ba4f551642f364a62e7a66a774aca99e637d558a
Lucas-luwh <lwh1561660826@gmail.com>
2020-11-22 14:50:46 +0800

    分类品种

--- src/main/java/com/mmall/dao/CategoryMapper.java
+++ src/main/java/com/mmall/dao/CategoryMapper.java
@@ -2,19 +2,38 @@ package com.mmall.dao;
 
 import com.mmall.pojo.Category;
 
+import java.util.List;
+
 /**
  * @author julin
  */
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     * 添加品种信息
+     * @param record 品种
+     * @return 响应信息
+     */
     int insert(Category record);
 
     int insertSelective(Category record);
 
     Category selectByPrimaryKey(Integer id);
 
+    /**
+     * 选择性更新分类信息
+     * @param record 分类
+     * @return 数量
+     */
     int updateByPrimaryKeySelective(Category record);
 
     int updateByPrimaryKey(Category record);
+
+    /**
+     * 获取当前节点下的子节点所有同级分类信息
+     * @param parentId 父级id
+     * @return 集合
+     */
+    List<Category> selectCategoryChildrenByParentId(Integer parentId);
 }
