a8f4fd8096641d146842e85c53761f8f178a2770
Lucas-luwh <lwh1561660826@gmail.com>
2020-12-02 23:13:19 +0800

    新增购物车模块功能

---
+++ src/main/java/com/mmall/common/RoleImpl.java
@@ -0,0 +1,19 @@
+package com.mmall.common;
+
+/**
+ * @package: com.mmall.common
+ * @author: luweihong
+ * @description:
+ * @create: 2020/12/2 22:05
+ * @version: 1.0
+ */
+public class RoleImpl implements Const.Role {
+	/**
+	 * 普通用户
+	 */
+	public static final int ROLE_CUSTOMER = 0;
+	/**
+	 * 管理员
+	 */
+	public static final int ROLE_ADMIN = 1;
+}
